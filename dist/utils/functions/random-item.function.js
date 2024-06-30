b558b516084142245c5c07f568e58e9e23072089
Warren Hawker <85311854+WarrenHawker@users.noreply.github.com>
2024-06-30 03:10:06 +0100

    Delete dist directory

--- dist/utils/functions/random-item.function.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.pickRandomItem = void 0;
-const pickRandomItem = (arr) => {
-    let randomIndex = Math.floor(Math.random() * arr.length);
-    return arr[randomIndex];
-};
-exports.pickRandomItem = pickRandomItem;
