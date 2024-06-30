0e2498d20647669aeca36aec6f248f24fdd758b1
WarrenHawker <hawker.warren@gmail.com>
2024-06-30 03:14:31 +0100

    DO SOMETHING!!!!

--- dist/lib/prisma/client.prisma.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.prismaClient = void 0;
-//import packages
-const client_1 = require("@prisma/client");
-//initialise prisma client
-exports.prismaClient = new client_1.PrismaClient();
