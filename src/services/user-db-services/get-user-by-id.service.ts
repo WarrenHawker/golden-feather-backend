72e11c2402ff63b929062f9ed2f8d65058c13cbe
WarrenHawker <hawker.warren@gmail.com>
2024-09-04 23:54:40 +0100

    removed password from fetched user

--- src/services/user-db-services/get-user-by-id.service.ts
+++ src/services/user-db-services/get-user-by-id.service.ts
@@ -4,6 +4,15 @@ export const getUserByIdDB = async (id: string) => {
   try {
     const user = prismaClient.user.findUnique({
       where: { id: id },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        role: true,
+        status: true,
+        created_on: true,
+        updated_on: true,
+      },
     });
     return user;
   } catch (error) {
