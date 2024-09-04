9ba5bb82968958c9af01b1b24643acb1c52ccf98
WarrenHawker <hawker.warren@gmail.com>
2024-09-04 20:22:22 +0100

    added get guild slug controller

--- src/routes/guild.route.ts
+++ src/routes/guild.route.ts
@@ -4,12 +4,14 @@ import { createGuild } from '../controllers/guild-controllers/create-guild.contr
 import { updateGuild } from '../controllers/guild-controllers/update-guild.controller';
 import { getGuilds } from '../controllers/guild-controllers/get-guilds.controller';
 import { checkRole, checkStatus } from '../middleware/require-auth.middleware';
+import { getGuildBySlug } from '../controllers/guild-controllers/get-guild-by-slug.controller';
 
 export const router = express.Router();
 
 router.get('/', getGuilds);
 router.post('/', checkRole('admin'), checkStatus('active'), createGuild);
 router.patch('/:id', updateGuild);
+router.get('/:slug', getGuildBySlug);
 router.delete('/:id', checkRole('admin'), checkStatus('active'), deleteGuild);
 
 //TODO fill out controller functions for all guild routes
