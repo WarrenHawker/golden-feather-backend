6b6ebaf599f92fa01fb1fbdd7a0b8cdb4d564e1e
WarrenHawker <hawker.warren@gmail.com>
2024-09-11 22:43:59 +0100

    fuck you

--- src/controllers/form-controllers/recieve-contact-form.controller.ts
+++ src/controllers/form-controllers/recieve-contact-form.controller.ts
@@ -2,10 +2,15 @@
 
 import { Request, Response } from 'express';
 import { ErrorReturn } from '../../types/error-return';
+import axios from 'axios';
 
 const receiveContactForm = async (req: Request, res: Response) => {
-  let { name, email, message } = req.body;
+  let { name, email, message, captchaToken } = req.body;
+  const recaptchaVerifyUrl = `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${captchaToken}`;
+
   try {
+    const captchaResponse = await axios.post(recaptchaVerifyUrl);
+    console.log(captchaResponse.data);
     return res.status(200).json(req.body);
   } catch (err) {
     const error: ErrorReturn = {
--- src/routes/route-files/form.route.ts
+++ src/routes/route-files/form.route.ts
@@ -8,11 +8,18 @@ import {
 import getForms from '../../controllers/form-controllers/get-forms.controller';
 import validateFields from '../../middleware/validate-fields.middleware';
 import receiveContactForm from '../../controllers/form-controllers/recieve-contact-form.controller';
+import rateLimiter from '../../middleware/rate-limiter.middleware';
 
 export const router = express.Router();
 
+//sending a contact form is rate limited to 3 requests per minute
 const contactFormFields = ['name', 'email', 'message'];
-router.post('/contact', validateFields(contactFormFields), receiveContactForm);
+router.post(
+  '/contact',
+  validateFields(contactFormFields),
+  rateLimiter(3, 60, 'contact-form'),
+  receiveContactForm
+);
 
 //all form GET routes require admin access
 router.use(checkSession());
