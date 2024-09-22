d1c8e312cedf13c73ca3991068452d84d4e540d3
WarrenHawker <hawker.warren@gmail.com>
2024-09-22 16:37:45 +0100

    testing stuff5

--- src/services/scheduled-tasks/tasks/sync-redis-task.service.ts
+++ src/services/scheduled-tasks/tasks/sync-redis-task.service.ts
@@ -35,13 +35,13 @@
 // export default syncRedisTask;
 
 import { Queue, Worker } from 'bullmq';
-import { IOredisClient } from '../../lib/redis/client.redis';
 import storeCreatorTagsRedis from '../redis-services/tag-redis-services/store-creator-tags-redis.service';
 import storeCreatorsRedis from '../redis-services/creator-redis-services/store-creators-redis.service';
 import storeGuildTagsRedis from '../redis-services/tag-redis-services/store-guild-tags-redis.service';
 import storeGuildsRedis from '../redis-services/guild-redis-services/store-guilds-redis.service';
 import storeLanguagesRedis from '../redis-services/language-redis-services/store-languages-redis.service';
 import storeRegionsRedis from '../redis-services/region-redis-services/store-regions-redis.service';
+import { IOredisClient } from '../../../lib/redis/client.redis';
 
 const redisConnect = {
   connection: IOredisClient,
