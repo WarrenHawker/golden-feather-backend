8ba732a1359c7c3004aa722bf08638a60dd4864d
WarrenHawker <hawker.warren@gmail.com>
2024-09-22 15:57:34 +0100

    added timeout to app.listen

--- src/server.ts
+++ src/server.ts
@@ -18,6 +18,7 @@ import { initializePrisma } from './lib/prisma/client.prisma';
 const port = process.env.PORT || 0;
 
 app.listen(port, async () => {
+  await new Promise((resolve) => setTimeout(resolve, 3000));
   try {
     //Initialize and connect to databases
     await initializeMongoDatabase();
